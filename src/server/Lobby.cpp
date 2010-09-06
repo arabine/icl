e36180ca903202161f8a1aa9bfd0704ffeba2b50
Anthony <belegar@users.noreply.github.com>
2010-09-06 23:50:28 +0200

    Comments

--- src/server/Lobby.cpp
+++ src/server/Lobby.cpp
@@ -1,3 +1,20 @@
+/*=============================================================================
+ * TarotClub - Lobby.cpp
+ *=============================================================================
+ * Manage temporary connections to join free game tables
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ *=============================================================================
+ */
 
 #include "Lobby.h"
 #include <iostream>
--- src/server/Lobby.h
+++ src/server/Lobby.h
@@ -1,10 +1,27 @@
+/*=============================================================================
+ * TarotClub - Lobby.h
+ *=============================================================================
+ * Manage temporary connections to join free game tables
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ *=============================================================================
+ */
+
 #ifndef LOBBY_H
 #define LOBBY_H
 
 #include <QtCore>
 #include "../TarotEngine.h"
 #include "ServerConfig.h"
-
 
 /*****************************************************************************/
 class Lobby : public QObject
@@ -30,7 +47,6 @@ public slots:
    void slotClientClosed();
    void slotReadData();
 };
-
 
 #endif // LOBBY_H
 
