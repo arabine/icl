f923106a8bbb0aa40c190898aa0473bf2c0e1481
arabine <arabine@CHA-L1018.itron.com>
2013-04-21 18:00:14 +0200

    Added full game simulation in the unit test

--- ai/beginner.js
+++ ai/beginner.js
@@ -37,11 +37,16 @@ var Place = {
 /*****************************************************************************/
 // UTILITY FUNCTIONS AND CLASSES
 
-function Card(cardName)
+/**
+ * This class allow you to specify an owner along with the card value
+ * If you don't provide the second argument, the owner will be set to HYPERSPACE
+ */
+function Card(cardName, place)
 {
 	var elem = cardName.split("-");
 	this.value = elem[0];
 	this.color = elem[1];
+	this.owner = place;
 
 // Methods	
 	this.GetName = function()
@@ -52,7 +57,20 @@ function Card(cardName)
 	// debug code only
 	this.Print = function()
 	{
-		SystemPrint("Card value: " + this.value + " Color: " + this.color);
+		var place;
+		
+		if (this.owner == Place.SOUTH) {
+			place = "South";
+		} else if (this.owner == Place.NORTH) {
+			place = "North";
+		} else if (this.owner == Place.WEST) {
+			place = "West";
+		} else if (this.owner == Place.EAST) {
+			place = "East";
+		} else {
+			place = "Nobody";
+		}
+		SystemPrint("Card value: " + this.value + " Color: " + this.color + " Owner: " + place);
 	}
 }
 
@@ -77,27 +95,21 @@ function Deck()
 		this.cards.length = 0;
 	}
 	
-	this.AddOneCard = function(cardName)
+	this.AddOneCard = function(cardName, place)
 	{
-		cards[cards.length] = new Card(cardName);
+		this.cards[this.cards.length] = new Card(cardName, place);
 	}
 	
-	this.AddCards = function(list)
+	this.SetCards = function(list)
 	{		
 		var result = list.split(/;/g);		
+		
+		this.Clear();
+		
 		for (var i=0; i<result.length; i++) {
 			this.cards[i] = new Card(result[i]);
 		}
 	}
-}
-
-/**
- * @brief This object will store statistics on each turn
- */ 
-function Turn()
-{
-	
-
 }
 
 /*****************************************************************************/
@@ -111,17 +123,27 @@ function Player()
 	this.hasClubs = false;
 	this.hasTrumps = false;
 	
+	// true if there is at least one suit missing, including trumps
+	this.missedSuits = false;
+	
 // Methods		
 	this.Initialize = function()
 	{
-		this.NoDiamond = false;
+		this.hasDiamonds = false;
+		this.hasSpades = false;
+		this.hasHearts = false;
+		this.hasClubs = false;
+		this.hasTrumps = false;
 	}
 	
 	this.Print = function()
 	{
-		SystemPrint("Has diamonds: " + this.hasDiamonds);
+		//SystemPrint("Has diamonds: " + this.hasDiamonds);
 	}
 }
+
+/*****************************************************************************/
+// MAIN BOT ENGINE
 
 function GameState()
 {
@@ -131,12 +153,13 @@ function GameState()
 		this.players[i] = new Player();
 	}
 	
-	this.deck = new Deck(); // deck of played cards
-	this.place = new Array(); // who has played
+	this.playedCards = new Array(18); // turns for 4 players
+	for(var i=0; i<18; i++) {
+		this.playedCards[i] = new Deck();
+	}
 	
-	this.gameCounter = 0;		// [1..72] with 4 players
 	this.turnCounter = 0;		// number of turns, 18 with 4 players
-	this.currentPosition = 0; 	// position of the current turn
+	this.currentPosition = 0; 	// position of the current turn [0..3] with 4 players
 	
 // Methods	
 	this.Initialize = function()
@@ -144,24 +167,39 @@ function GameState()
 		for(var i=0; i<this.players.length; i++) {
 			this.players[i].Initialize();
 		}
-		gameCounter = 0;
-		deck.Clear();
+		this.turnCounter = 0;
+		this.currentPosition = 0;
+		
+		for(var i=0; i<this.playedCards.length; i++) {
+			this.playedCards[i].Clear();
+		}
 	}
 	
 	this.Print = function()
 	{
-		for(var i=0; i<this.players.length; i++) {
-			this.players[i].Print();
+		this.currentPosition = 0;
+		this.turnCounter = 0;
+		
+		for(var i=0; i<18; i++) {
+			var j = i+1;
+			SystemPrint("-- Tour --" + j);
+			this.playedCards[i].Print();
 		}
 	}
 	
-	this.CardPlayed = function(cardName, place)
+	this.SetPlayedCard = function(cardName, place)
 	{
-		
+		this.playedCards[this.turnCounter].AddOneCard(cardName, place);
+	
+		this.currentPosition++;
+		if (this.currentPosition >= 4) {
+			this.currentPosition = 0;
+			this.turnCounter++;
+		}
 	}
 }
 
-CurrentGame = new GameState();
+var CurrentGame = new GameState();
 
 
 /*****************************************************************************/
@@ -169,24 +207,64 @@ CurrentGame = new GameState();
 
 function SystemPrint(message)
 {
-	//document.write(message + "<br />");
-	print(message + "<br />");
+	document.write(message + "<br />");
+	//print(message + "<br />");
 }
 
 function RunUnitTest()
 {
+	SystemPrint("********** START UNIT TEST **********");
 	myCard = new Card("7-C");
 	deck = new Deck();
-	deck.AddCards("7-C;21-A");
+	deck.SetCards("7-C;21-A");
 	deck.Print();
 	deck.Clear();
 	deck.Print();
 	
+	SystemPrint(""); // empty string = skip line
+}
+
+function CreateFakeGame()
+{
+	// FIXME: load string from generated file
+	var cardList = "13-A;15-A;19-A;21-A;10-A;20-A;3-A;5-A;7-D;9-D;11-D;14-D;11-C;8-C;13-C;4-C;9-S;6-S;12-S;11-S;5-H;13-H;8-H;12-H;16-A;6-A;12-A;4-A;8-A;6-D;9-A;1-A;1-C;7-C;14-C;9-C;14-A;3-H;7-A;2-C;11-A;12-D;2-A;10-H;7-H;4-H;6-H;1-H;3-C;12-C;5-C;6-C;5-S;14-S;13-S;0-A;3-S;10-S;18-A;7-S;5-D;3-D;8-S;10-D;9-H;14-H;4-S;2-H;17-A;1-D;2-S;1-S";
+	var ownerList = "1;2;3;0;0;1;2;3;1;2;3;0;0;1;2;3;2;3;0;1;0;1;2;3;1;2;3;0;1;2;3;0;3;0;1;2;1;2;3;0;1;2;3;0;1;2;3;0;1;2;3;0;2;3;0;1;3;0;1;2;1;2;3;0;0;1;2;3;1;2;3;0;";
+	var dogList = "6-C;1-C;1-S;12-D;11-C;13-H";
+	
+	this.cards = cardList.split(/;/g);
+	this.owners = ownerList.split(/;/g);
+	this.dog = dogList.split(/;/g);
+	
+	SystemPrint("\n********** NEW GAME SIMULATOR **********");
 	CurrentGame.Initialize();
+	
+	// Send the cards to the AI engine
+	for(var i=0; i<72; i++) {
+		CurrentGame.SetPlayedCard(this.cards[i], this.owners[i]);
+	}
+	
 	CurrentGame.Print();
+	
+	this.ReadCSV = function(locfile) {
+		// load a whole csv file, and then split it line by line
+		var req = new XMLHttpRequest();
+		req.open("POST", locfile, false);
+		req.send("");
+		return req.responseText.split(/\n/g);
+	}
+
+}
+
+function RunFakeGame()
+{
+	this.fakeGame = new CreateFakeGame();
+
+
 }
 
 RunUnitTest();
+RunFakeGame();
+
 
 
 /*****************************************************************************/
@@ -237,10 +315,10 @@ function PlayCard(gameCounter)
 //	debugger;
 
     myDeck = new Deck();
-	myDeck.AddCards(TDeck.GetBotCards());
+	myDeck.SetCards(TDeck.GetBotCards());
 	
 	mainDeck = new Deck();
-	mainDeck.AddCards(TDeck.GetMainCards());
+	mainDeck.SetCards(TDeck.GetMainCards());
 		
 	return name = myDeck.cards[0].GetName();
 }
