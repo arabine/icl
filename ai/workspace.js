a4d1a7d285847d7bf2d20d61e392c425c4a66cad
arabine <arabine@CHA-L1018.itron.com>
2013-07-27 19:19:55 +0200

    Unused file.

--- ai/workspace.js
+++
@@ -1,9 +0,0 @@
-<NotepadPlus>
-    <Project name="TarotClub">
-        <File name="beginner.js" />
-        <File name="index.html" />
-        <File name="round_cards.csv" />
-        <File name="round_cards.txt" />
-        <File name="unit_test.js" />
-    </Project>
-</NotepadPlus>
