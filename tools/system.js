b0bb6406a300f50bcec0928d8590760e6c1f2ec0
arabine <arabine@CHA-L1018.itron.com>
2013-07-27 19:20:25 +0200

    Mass move/renaming

---
+++ tools/system.js
@@ -0,0 +1,31 @@
+/*=============================================================================
+ * TarotClub - System.js
+ *=============================================================================
+ * Definition of system functions
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
+function SystemPrint(message)
+{
+	document.write(message + "<br />");
+}
+
+// End of file
