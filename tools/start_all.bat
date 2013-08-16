1478b70f234687b1c7a569299976e15eff577ae0
Anthony <Anthony@Moria.home>
2013-08-16 21:11:16 +0200

    Added debug script

---
+++ tools/start_all.bat
@@ -0,0 +1,16 @@
+
+copy /Y "bin\TarotClub.exe" "bin - Bender"
+cd "bin - Bender"
+start TarotClub.exe"
+cd ..
+
+copy /Y "bin\TarotClub.exe" "bin - Fry"
+cd "bin - Fry"
+start TarotClub.exe"
+cd ..
+
+copy /Y "bin\TarotClub.exe" "bin - Zoidberg"
+cd "bin - Zoidberg"
+start TarotClub.exe"
+cd ..
+
