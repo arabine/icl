7c01d771965e73b4c88c22b6803da7dcad87d774
Anthony Rabine <anthony.rabine@tarotclub.fr>
2013-11-13 22:23:14 +0100

    New class: logging system

---
+++ src/Log.h
@@ -0,0 +1,78 @@
+/*=============================================================================
+ * TarotClub - Log.h
+ *=============================================================================
+ * Log utility class: store events and data in a chronological way
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <QFile>
+#include <QMutex>
+#include "defines.h"
+
+/**
+ * @brief The Log class
+ *
+ * File format is CSV, each new action will append a new entry at the end of the file.
+ *
+ * "Action", "Date", "Key", "Value"
+ *
+ * Example:
+ * Error, 19-04-2013 09:17:30, "Protocol", "",
+ *
+ * Supported Actions:
+ *  - Error
+ *  - Info
+ *  - Game
+ *  - Message
+ *
+ */
+class Log
+{
+public:
+    enum Event
+    {
+        Error = 0,
+        Info = 1,
+        Engine = 2,
+        Bot = 3,
+        Protocol = 4,
+        Message = 5
+    };
+
+    Log();
+
+    static void AddEntry(Event event, const QString &key, const QString &value);
+
+private:
+    static QMutex mMutex;
+
+    static void Save(const QString &line);
+
+};
+
+#endif // LOG_H
+
+//=============================================================================
+// End of file Log.h
+//=============================================================================
