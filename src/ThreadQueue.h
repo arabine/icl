73963dabd57098d0285e414547c98b42a06d6c26
Anthony Rabine <rabine.anthony@gmail.com>
2013-12-01 22:21:33 +0100

    Comments

--- src/ThreadQueue.h
+++ src/ThreadQueue.h
@@ -1,3 +1,28 @@
+/*=============================================================================
+ * TarotClub - ThreadQueue.h
+ *=============================================================================
+ * Generic, blocking queue of data, to be used within a thread
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
 #ifndef THREADQUEUE_H
 #define THREADQUEUE_H
 
@@ -8,8 +33,9 @@
 #include <mutex>
 #include <chrono>
 
+
 template<typename Data>
-class ConcurrentQueue
+class ThreadQueue
 {
 
 public:
@@ -59,5 +85,8 @@ private:
 
 };
 
-
 #endif // THREADQUEUE_H
+
+//=============================================================================
+// End of file ThreadQueue.h
+//=============================================================================
