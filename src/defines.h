a1028997c3d4539d1ab0ed86b27608ffbda5a021
Anthony Rabine <rabine.anthony@gmail.com>
2014-01-10 22:19:59 +0100

    Updated version

--- src/defines.h
+++ src/defines.h
@@ -38,7 +38,7 @@
  * @brief The version string uses Semantic Versioning format
  * @see http://semver.org
  */
-#define TAROT_VERSION   "2.3.0-alpha.1"
+#define TAROT_VERSION   "2.3.0-alpha.2"
 #define TAROT_TITRE     "TarotClub"
 #define TAROT_VNAME     "Christmas"
 
