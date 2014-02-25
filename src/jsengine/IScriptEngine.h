ae97c3edd16e7a547e266424ded65399082aaab3
Anthony Rabine <rabine.anthony@gmail.com>
2014-02-25 17:25:12 +0100

    Added header/footer comments

--- src/jsengine/IScriptEngine.h
+++ src/jsengine/IScriptEngine.h
@@ -1,3 +1,28 @@
+/*=============================================================================
+ * TarotClub - IScriptEngine.h
+ *=============================================================================
+ * Agnostic script engine interface
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
 #ifndef I_SCRIPT_ENGINE_H
 #define I_SCRIPT_ENGINE_H
 
@@ -5,6 +30,7 @@
 #include <vector>
 #include "JSValue.h"
 
+/*****************************************************************************/
 class IScriptEngine
 {
 public:
@@ -17,3 +43,7 @@ public:
 };
 
 #endif // I_SCRIPT_ENGINE_H
+
+//=============================================================================
+// End of file IScriptEngine.h
+//=============================================================================
