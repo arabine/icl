23e1ca4c8f1e141ebd00420594fc1a3dfa24c388
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-03-28 17:57:23 +0100

    Added script and deck path to manage platform specificities

--- src/System.cpp
+++ src/System.cpp
@@ -10,27 +10,44 @@
 
 /*****************************************************************************/
 std::string System::mHomePath;
+std::string System::mDeckPath;
+#ifdef TAROT_DEBUG
+std::string System::mScriptPath;
+#endif
+
+/*****************************************************************************/
+std::string System::ScriptPath()
+{
+#ifdef TAROT_DEBUG
+    return mScriptPath;
+#else
+    return mHomePath + "/ai";
+#endif
+}
 /*****************************************************************************/
 void System::Initialize()
 {
-
 // ----------- Android
 #ifdef USE_ANDROID_OS
     QString path = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
     mHomePath = path.toStdString();
 
+    FIXME: Copy script files from the Qt resource file to the application internal memory
+
+    mDeckPath = ":cards/default/";
 // ----------- Linux, Windows ...
 #else
 
 #ifdef TAROT_DEBUG
     mHomePath  = "./";
+    mScriptPath = Util::ExecutablePath() + "/../../assets/ai/";
+    mDeckPath = Util::ExecutablePath() + "/../../assets/cards/default/";
 #else
     mHomePath  = Util::HomePath() + "/.tarotclub/";
 #endif
 #endif
 
-    // Check user TarotClub directories and create them if necessary (all platforms)
-
+    // Check the user TarotClub directories and create them if necessary (all platforms)
     if (!Util::FolderExists(HomePath()))
     {
         Util::Mkdir(HomePath());
--- src/System.h
+++ src/System.h
@@ -7,10 +7,16 @@
 class System
 {
 public:
-    static std::string HomePath() { return mHomePath; }
-    static std::string GamePath() { return mHomePath + "/games/"; }
-    static std::string LogPath() { return mHomePath + "/logs/"; }
-    static std::string ScriptPath() { return  mHomePath + "/ai"; }
+    static std::string HomePath()   { return mHomePath; }
+    static std::string GamePath()   { return mHomePath + "/games/"; }
+    static std::string LogPath()    { return mHomePath + "/logs/"; }
+
+    /**
+     * @brief DeckPath, where the card images are located
+     * @return The path
+     */
+    static std::string DeckPath()   { return mDeckPath; }
+    static std::string ScriptPath();
 
     static void Initialize();
 
@@ -18,6 +24,11 @@ private:
     System(); // forbid any instance
 
     static std::string mHomePath;
+    static std::string mDeckPath;
+
+#ifdef TAROT_DEBUG
+    static std::string mScriptPath;
+#endif
 };
 
 #endif // SYSTEM_H
