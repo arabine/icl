1f09080a12c2496fd07d77edec8b2c0dd7e68476
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-04-05 14:50:22 +0200

    Comments

--- src/System.cpp
+++ src/System.cpp
@@ -1,3 +1,27 @@
+/*=============================================================================
+ * TarotClub - System.cpp
+ *=============================================================================
+ * Manages system path for resources and generated files
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
 
 #ifdef USE_ANDROID_OS
 #include <QStandardPaths>
@@ -69,3 +93,7 @@ void System::Initialize()
 System::System()
 {
 }
+
+//=============================================================================
+// End of file System.cpp
+//=============================================================================
--- src/System.h
+++ src/System.h
@@ -1,5 +1,29 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
+/*=============================================================================
+ * TarotClub - System.h
+ *=============================================================================
+ * Manages system path for resources and generated files
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
 
 #include <string>
 
@@ -31,4 +55,8 @@ private:
 #endif
 };
 
-#endif // SYSTEM_H
+#endif // SYSTEM_H_
+
+//=============================================================================
+// End of file System.h
+//=============================================================================
