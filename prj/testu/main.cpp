d9b311d7d50ed805ec3d5302499f21b73f266b1c
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-07-03 18:41:09 +0200

    Added summary of success and failures at the end of execution

--- prj/testu/main.cpp
+++ prj/testu/main.cpp
@@ -1,6 +1,8 @@
 
 #include <QtCore>
 #include <QtTest>
+#include <cstdint>
+#include <iostream>
 
 #include "tst_tarot_base.h"
 #include "tst_utilities.h"
@@ -9,19 +11,56 @@
 
 int main(int argc, char *argv[])
 {
+    std::uint32_t testSuccesses = 0;
+    std::uint32_t testFailures = 0;
+
     QCoreApplication app(argc, argv);
     app.setAttribute(Qt::AA_Use96Dpi, true);
     Utilities tst_utils;
-    QTest::qExec(&tst_utils, argc, argv);
+    if (QTest::qExec(&tst_utils, argc, argv) == 0)
+    {
+        testSuccesses++;
+    }
+    else
+    {
+        testFailures++;
+    }
 
     TarotBase tst_base;
-    QTest::qExec(&tst_base, argc, argv);
+    if (QTest::qExec(&tst_base, argc, argv) == 0)
+    {
+        testSuccesses++;
+    }
+    else
+    {
+        testFailures++;
+    }
 
     JsonTest tst_json;
-    QTest::qExec(&tst_json, argc, argv);
+    if (QTest::qExec(&tst_json, argc, argv) == 0)
+    {
+        testSuccesses++;
+    }
+    else
+    {
+        testFailures++;
+    }
 
     LibraryHash tst_hash;
-    QTest::qExec(&tst_hash, argc, argv);
+    if (QTest::qExec(&tst_hash, argc, argv) == 0)
+    {
+        testSuccesses++;
+    }
+    else
+    {
+        testFailures++;
+    }
+
+    std::cout << std::endl << "=============================================" << std::endl;
+    std::cout << std::endl << "Success : " << testSuccesses << std::endl;
+    std::cout << std::endl << "Failures: " << testFailures << std::endl;
+    std::cout << std::endl << "=============================================" << std::endl;
+
 
     return 0;
 }
