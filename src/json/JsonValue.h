f0bab156bcdb376b5bf72737b8ef2dbe81b4133b
Anthony Rabine <rabine.anthony@gmail.com>
2014-07-06 21:38:40 +0200

    Coding style applied

--- src/json/JsonValue.h
+++ src/json/JsonValue.h
@@ -1,3 +1,28 @@
+/*=============================================================================
+ * TarotClub - JsonValue.h
+ *=============================================================================
+ * Modelization of a JSON generic value
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
 #ifndef JSONVALUE_H
 #define JSONVALUE_H
 
@@ -14,7 +39,9 @@ public:
 /*****************************************************************************/
 class JsonValue : public JSValue, public IJsonNode
 {
+
 public:
+
     JsonValue(std::int32_t value) : JSValue(value) {}
     JsonValue(double value) : JSValue(value) {}
     JsonValue(const char *value) : JSValue(value) {}
@@ -27,3 +54,7 @@ public:
 };
 
 #endif // JSONVALUE_H
+
+//=============================================================================
+// End of file JsonValue.h
+//=============================================================================
