d85432ac09f1fc436171f77d281b1bfeb78b63f4
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-07-31 17:46:16 +0200

    Added unit test of the WebSocket upgrade class (not working with GCC because of a lack of C++0x11 regex);

--- prj/testu/tst_tcp_socket.cpp
+++ prj/testu/tst_tcp_socket.cpp
@@ -112,42 +112,6 @@ public:
 
 int main()
 {
-
-#ifdef TEST_WEBSOCKET_HANDSHAKE
-
-	// Test from RFC 6455...
-	//$key=         "dGhlIHNhbXBsZSBub25jZQ=="
-	//$cat_string=  "dGhlIHNhbXBsZSBub25jZQ==258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-	//$resultBase64="s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
-
-	std::string data = "GET / HTTP / 1.1\r\n";
-	data += "Host: server.example.com\r\n";
-	data += "Upgrade: websocket\r\n";
-	data += "Connection: Upgrade\r\n";
-	data += "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
-	data += "Origin: http://example.com\r\n";
-	data += "Sec-WebSocket-Protocol: superchat\r\n";
-
-	WebSocketRequest req;
-	req.Parse(data);
-
-	if (req.IsValid())
-	{
-		std::string accept = req.Accept();
-		if (accept == "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=")
-		{
-			std::cout << "Upgrade header: " << std::endl << req.Upgrade("tarotclub") << std::endl;
-		}
-		else
-		{
-			std::cout << "Invalid accept encoded string: " << accept << std::endl;
-		}
-	}
-	else
-	{
-		std::cout << "Invalid request" << std::endl;
-	}
-#endif
     std::thread clientThread;
 
 	// Declare tcp server callbacks
