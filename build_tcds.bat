6706e956b7929bbee231215845102fc6934a8cab
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-08-12 08:25:24 +0200

    Renamed makefile (tcds extension)

--- build_tcds.bat
+++ build_tcds.bat
@@ -3,4 +3,6 @@ REM This batch script will build a release version of the TarotClub Dedicated Se
 REM It is intended to be run under a Windows command prompt.
 REM The following software must be available in the PATH: mingw32-make, MSYS, g++ (MinGW)
 
-mingw32-make -j4 ARCH=mingw %1
+REM Batch command line argument is passed to the makefile
+REM Example: build_tcds.bat clean
+mingw32-make -f Makefile.tcds -j4 ARCH=mingw %1
