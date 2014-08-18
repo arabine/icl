a62ee4de6ca3eacfbeeec31edbc4399dd9c88dbd
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-08-18 21:33:03 +0200

    Added options

--- quality/run_vera++.bat
+++ quality/run_vera++.bat
@@ -4,4 +4,4 @@ REM Delete old file
 rm -f %CD%\report.txt
 
 perl "vera++\generate_list.pl" %CD%\..
-"C:\Program Files (x86)\vera++\bin\vera++.exe" -r %CD%\vera++ -s -i %CD%\files.txt -o %CD%\report.txt -P max-line-length=150
+"C:\Program Files (x86)\vera++\bin\vera++.exe" -r %CD%\vera++ -s -i %CD%\files.txt -o %CD%\report.txt -P max-line-length=180
