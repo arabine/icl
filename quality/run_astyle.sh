700a0c6b8f05bca55beaccdc861815eeb9f56794
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-08-18 16:35:12 +0200

    Moved AStyle into quality directory

---
+++ quality/run_astyle.sh
@@ -0,0 +1,4 @@
+# Run ASyle on the entire TarotClub source code
+
+astyle --options=astyle/astyle.cfg ../src/*.cpp ../src/*.h ../src/library/*.cpp ../src/library/*.h ../src/json/*.cpp ../src/json/*.h
+
--- tools/run_astyle.sh
+++
@@ -1,4 +0,0 @@
-# Run asyle on the entire TarotClub source code
-
-astyle --options=astyle.cfg ../src/*.cpp ../src/*.h ../src/library/*.cpp ../src/library/*.h ../src/json/*.cpp ../src/json/*.h
-
