b3a22613e91e8cd978ba28f15817fae3bebcd55d
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-10-14 14:39:35 +0200

    Added command line bot client project

---
+++ src/botclient/main.cpp
@@ -0,0 +1,84 @@
+/*=============================================================================
+ * TarotClub - main.cpp
+ *=============================================================================
+ * Entry point of TarotClub dedicated server
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
+#include "Lobby.h"
+#include "System.h"
+#include "GetOptions.h"
+#include "ClientConfig.h"
+
+/*****************************************************************************/
+/**
+ * @brief Entry point of the dedicated bot client
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    if(CmdOptionExists(argv, argv + argc, "-h"))
+    {
+        // Print help and exit:
+        std::cout << std::endl << "Usage: " << argv[0] << "[option] [argument] ..." << std::endl;
+        std::cout << "Options are:" << std::endl;
+        std::cout << "\t" << "-h" << "\tPrints this help" << std::endl;
+        std::cout << "\t" << "-f" << "\tSpecifies a JSON client configuration file" << std::endl;
+        std::cout << "\t" << "-c" << "\tConnect to the specified server IP address" << std::endl;
+        std::cout << "\t" << "-p" << "\tSpecifies the server TCP port number" << std::endl;
+        return 0;
+    }
+
+    // Parse arguments
+    std::string fileName(GetCmdOption(argv, argv + argc, "-f"));
+    std::string address(GetCmdOption(argv, argv + argc, "-c"));
+    std::string port(GetCmdOption(argv, argv + argc, "-p"));
+
+    ClientConfig conf;
+    conf.Load(fileName); // will load default settings if fileName is not valid
+
+    System::Initialize();
+    Log::SetLogPath(System::LogPath());
+
+    std::cout << "TarotClub bot client " << TAROT_VERSION << " is starting." << std::endl;
+
+    Bot bot;
+
+    bot.SetIdentity(conf.GetOptions().identity);
+    bot.SetTimeBeforeSend(0U);
+    bot.Initialize();
+    // Connect the bot to the server
+    bot.ConnectToHost(address, std::stoi(port));
+
+
+    std::cout << "Hit the enter button to exit the client" << std::endl;
+
+    std::cin.get();
+    bot.Close();
+
+    return 0;
+}
+
+//=============================================================================
+// End of file main.cpp
+//=============================================================================
