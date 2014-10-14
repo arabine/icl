37cc2ac51c95fae8927a040c2d474d418c3314df
Anthony Rabine <anthony.rabine@tarotclub.fr>
2014-10-14 14:37:56 +0200

    Added file headers

--- src/library/GetOptions.h
+++ src/library/GetOptions.h
@@ -1,3 +1,28 @@
+/*=============================================================================
+ * TarotClub - GetOptions.h
+ *=============================================================================
+ * Retrieve the command line option parameters
+ *=============================================================================
+ * TarotClub ( http://www.tarotclub.fr ) - This file is part of TarotClub
+ * Copyright (C) 2003-2999 - Anthony Rabine
+ * anthony@tarotclub.fr
+ *
+ * TarotClub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TarotClub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TarotClub.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *=============================================================================
+ */
+
 #ifndef GETOPTIONS_H
 #define GETOPTIONS_H
 
@@ -19,3 +44,7 @@ bool CmdOptionExists(char** begin, char** end, const std::string& option)
 }
 
 #endif // GETOPTIONS_H
+
+//=============================================================================
+// End of file GetOptions.h
+//=============================================================================
