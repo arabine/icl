d099af136d3d4be6dcbd617379e09faf48f9cb32
Anthony Rabine <anthony.rabine@tarotclub.fr>
2015-02-12 08:36:55 +0100

    Changed default Web port to avoid conflict with the database

--- src/ServerConfig.h
+++ src/ServerConfig.h
@@ -54,7 +54,7 @@ class ServerConfig
 public:
     static const std::uint16_t  DEFAULT_DELAY               = 500U;     // in ms
     static const std::uint16_t  DEFAULT_GAME_TCP_PORT       = 4269U;
-    static const std::uint16_t  DEFAULT_WEB_TCP_PORT        = 8080U;
+    static const std::uint16_t  DEFAULT_WEB_TCP_PORT        = 8090U;
     static const std::uint32_t  DEFAULT_LOBBY_MAX_CONN      = 250U;
     static const std::string    DEFAULT_SERVER_CONFIG_FILE;
     static const std::uint8_t   DEFAULT_NUMBER_OF_TURNS     = 5U;
