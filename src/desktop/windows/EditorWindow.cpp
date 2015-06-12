6732056ce18f1d08517fd5dc5b295eb3334ac246
Anthony Rabine <anthony.rabine@tarotclub.fr>
2015-06-12 10:46:37 +0200

    Corrected build

--- src/desktop/windows/EditorWindow.cpp
+++ src/desktop/windows/EditorWindow.cpp
@@ -38,7 +38,7 @@ EditorWindow::EditorWindow(QWidget *parent)
     connect(ui.openButton, SIGNAL(clicked()), this, SLOT(slotOpenDeal()));
     connect(ui.saveButton, SIGNAL(clicked()), this, SLOT(slotSaveDeal()));
     connect(ui.buttonClear, &QPushButton::clicked, this, &EditorWindow::slotClear);
-    connect(ui.closeButton, &QPushButton::clicked, this, EditorWindow::slotClose);
+    connect(ui.closeButton, &QPushButton::clicked, this, &EditorWindow::slotClose);
 
     // Arrow to dispatch cards
     connect(ui.toSouth, SIGNAL(clicked()), this, SLOT(slotToSouth()));
