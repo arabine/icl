ac2596b172745aee2ed188eee992fad17f196bcf
belegar <anthony.rabine@tarotclub.fr>
2016-09-13 17:25:50 +0200

    Added tests

---
+++ tests/tst_json.h
@@ -0,0 +1,25 @@
+#ifndef TST_JSENGINE_H
+#define TST_JSENGINE_H
+
+#include <QString>
+#include <QtTest>
+#include <QCoreApplication>
+#include <cstdint>
+
+class JsonTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    JsonTest();
+
+private Q_SLOTS:
+    void ParseFile();
+    void ModifyValue();
+    void GenerateFile();
+
+private:
+
+};
+
+#endif // TST_JSENGINE_H
