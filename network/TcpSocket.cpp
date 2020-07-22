4c75ca2e7231a864c754a5296cc97354b42a8eac
Anthony Rabine <anthony@rabine.fr>
2020-07-22 16:50:31 +0200

    Continue on sig interrupt

--- network/TcpSocket.cpp
+++ network/TcpSocket.cpp
@@ -112,7 +112,8 @@ bool TcpSocket::AnalyzeSocketError(const char* context)
     e = errno;
     const char* msg = strerror( e );
     if ((e == EAGAIN) // equals EWOULDBLOCK
-        || (e == EINPROGRESS))
+        || (e == EINPROGRESS)
+        || (e == EINTR))
     {
         ok = true;
     }
